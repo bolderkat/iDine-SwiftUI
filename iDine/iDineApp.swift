a65f929e9515f637071cc7e06a5af4e590115db2
Daniel Luo <d.luo117@gmail.com>
2021-03-08 16:22:24 -0800

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Daniel Luo on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
