393154566274ab6ccbbd5db49c959f4c43ef7c38
Daniel Luo <d.luo117@gmail.com>
2021-03-09 16:44:14 -0800

    Allow for item removal

--- iDine/OrderView.swift
+++ iDine/OrderView.swift
@@ -21,16 +21,25 @@ struct OrderView: View {
                             Text("$\(item.price)")
                         }
                     }
+                    .onDelete(perform: deleteItems)
                 }
                 Section {
                     NavigationLink(destination: CheckoutView()) {
                         Text("Place Order")
                     }
                 }
+                .disabled(order.items.isEmpty)
             }
             .navigationTitle("Order")
             .listStyle(InsetGroupedListStyle())
+            .toolbar(content: {
+                EditButton()
+            })
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        order.items.remove(atOffsets: offsets)
     }
 }
 
